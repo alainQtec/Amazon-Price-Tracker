e4b04eebf3f75ca36fcc4727209aafdb3c1094da
Alain Herve <alainQtec@gmail.com>
2023-04-28 13:32:14 +0200

    Add main logic

--- background.js
+++ background.js
@@ -1,3 +1,37 @@
 /**
  *  This file contains the logic for injecting the button into Amazon product pages and handling user input.
  */
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.storage.sync.set({ products: [] });
+});
+
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.action === "addProduct") {
+        chrome.storage.sync.get("products", (data) => {
+            const products = data.products;
+            products.push(request.product);
+            chrome.storage.sync.set({ products }, () => {
+                sendResponse({ message: "Product added successfully!" });
+            });
+        });
+    } else if (request.action === "getProducts") {
+        chrome.storage.sync.get("products", (data) => {
+            sendResponse({ products: data.products });
+        });
+    } else if (request.action === "removeProduct") {
+        chrome.storage.sync.get("products", (data) => {
+            const products = data.products.filter(
+                (product) => product.id !== request.productId
+            );
+            chrome.storage.sync.set({ products }, () => {
+                sendResponse({ message: "Product removed successfully!" });
+            });
+        });
+    }
+});
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
+    if (changeInfo.status === "complete" && /^https?:\/\/www\.amazon\..*/.test(tab.url)) {
+        chrome.tabs.executeScript(tabId, { file: "content.js" });
+    }
+});
--- popup.js
+++ popup.js
@@ -1,3 +1,63 @@
 /**
  * This file contains the logic for communicating with the background script and updating the list of products being tracked.
  */
+function renderProduct(product) {
+    const productEl = document.createElement("div");
+    productEl.classList.add("product");
+
+    const imgEl = document.createElement("img");
+    imgEl.src = product.image;
+    imgEl.alt = product.name;
+    productEl.appendChild(imgEl);
+
+    const nameEl = document.createElement("div");
+    nameEl.classList.add("name");
+    nameEl.textContent = product.name;
+    productEl.appendChild(nameEl);
+
+    const priceEl = document.createElement("div");
+    priceEl.classList.add("price");
+    priceEl.textContent = `Current price: $${product.price}`;
+    productEl.appendChild(priceEl);
+
+    const desiredPriceEl = document.createElement("div");
+    desiredPriceEl.classList.add("desired-price");
+    desiredPriceEl.textContent = `Desired price: $${product.desiredPrice}`;
+    productEl.appendChild(desiredPriceEl);
+
+    const intervalEl = document.createElement("div");
+    intervalEl.classList.add("interval");
+    intervalEl.textContent = `Check interval: ${product.interval} minutes`;
+    productEl.appendChild(intervalEl);
+
+    const removeBtn = document.createElement("button");
+    removeBtn.classList.add("remove-btn");
+    removeBtn.textContent = "Remove";
+    removeBtn.addEventListener("click", () => {
+        chrome.runtime.sendMessage(
+            { action: "removeProduct", productId: product.id },
+            (response) => {
+                if (response.message) {
+                    alert(response.message);
+                }
+            }
+        );
+        productEl.remove();
+    });
+    productEl.appendChild(removeBtn);
+
+    return productEl;
+}
+
+function renderProductList(products) {
+    const productList = document.getElementById("product-list");
+    productList.innerHTML = "";
+
+    products.forEach((product) => {
+        productList.appendChild(renderProduct(product));
+    });
+}
+
+chrome.runtime.sendMessage({ action: "getProducts" }, (response) => {
+    renderProductList(response.products);
+});
